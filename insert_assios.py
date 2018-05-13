e148f72ec5ec08111235ded670e89110619a8039
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-05-13 16:39:22 +0200

    m

--- insert_assios.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-con = sql.connect("./database.db")
-cur = con.cursor()
-email = "asbjorn@steinskog.me"
-tok = "jada"
-cur.execute("INSERT INTO emails (email, token) VALUES (?,?)", (email, tok))
-con.commit()
-cur.close()
